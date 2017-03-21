f62057e30dde498928b3e3e6a8a940f2bd49aaa9
Tristan Edwards <tristan.edwards@me.com>
2017-03-21 12:10:45 +0100

    Handle invalid users

--- lib/messengyr/web/views/api/user_view.ex
+++ lib/messengyr/web/views/api/user_view.ex
@@ -29,14 +29,22 @@ defmodule Messengyr.Web.UserView do
     }
   end
 
-  def user_json(user) do
-    hash_email = :crypto.hash(:md5, user.email) |> Base.encode16 |> String.downcase 
+  def user_json(%{ email: email } = user) do
+    hash_email = :crypto.hash(:md5, email) |> Base.encode16 |> String.downcase 
     avatar_url = "http://www.gravatar.com/avatar/#{hash_email}"
 
     %{
       id: user.id,
       username: user.username,
       avatarURL: avatar_url,
+    }
+  end
+
+  def user_json(user) do
+    IO.inspect user
+
+    %{
+      
     }
   end
 end
